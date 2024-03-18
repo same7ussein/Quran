472ca664d08a6dd68f93bd703d620621110cac42
sameh hussien <riotteam61@gmail.com>
2024-03-18 21:23:06 +0200

    create project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { SearchPipe } from './shared/interceptors/search.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchPipe
   ],
   imports: [
     BrowserModule,
