90a2a806723e9a23673ffb5a0b05e2df764f07ea
Youssef Abdulluh <151579855+youssefabdullah08@users.noreply.github.com>
2024-03-19 23:05:05 +0200

    handling logic of sura youssef

--- src/app/components/reading-sura/reading-sura.component.ts
+++ src/app/components/reading-sura/reading-sura.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Sura } from './../../shared/interfaces/sura';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QuranService } from 'src/app/shared/services/quran.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-reading-sura',
@@ -8,6 +11,33 @@ import { CommonModule } from '@angular/common';
   templateUrl: './reading-sura.component.html',
   styleUrls: ['./reading-sura.component.scss']
 })
-export class ReadingSuraComponent {
+export class ReadingSuraComponent implements OnInit {
+constructor(private _QuranService:QuranService , private _ActivatedRoute:ActivatedRoute){}
+suraId:any=0
+suraopj:Sura={} as Sura
+ngOnInit(): void {
+    this.getSuraId()
+    this.getSpecialSura()
+}
+
+getSuraId():void{
+this._ActivatedRoute.paramMap.subscribe({
+  next:(res)=>{
+    console.log(res.get('id'));
+    this.suraId=res.get('id');
+    
+  }
+})
+}
+
+getSpecialSura():void{
+  this._QuranService.SpecialQuran(this.suraId).subscribe({
+    next:(res)=>{
+      console.log(res.verses);
+      this.suraopj=res.verses
+    }
+  })
+}
+
 
 }
